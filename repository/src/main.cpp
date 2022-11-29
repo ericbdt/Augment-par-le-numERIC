974f700b93d7611e60f6aa79572cbea76a2a4e6c
ericbdt <eric@LAPTOP-6DVD499O.localdomain>
2022-11-29 11:20:24 +0100

    Update in cmake repository's direction

--- repository/src/main.cpp
+++ repository/src/main.cpp
@@ -1,7 +1,23 @@
 #include <cstdio>
+#include <SFML/Window.hpp>
 
 int main()
 {
+
+    sf::Window window(sf::VideoMode(800,600), "My window");
+    while (window.isOpen())
+        {
+            // on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
+            sf::Event event;
+            while (window.pollEvent(event))
+            {
+                // évènement "fermeture demandée" : on ferme la fenêtre
+                if (event.type == sf::Event::Closed)
+                    window.close();
+            }
+        }
+
+
     printf("sheesh\n");
     return (0);
 }
