cadaa4b644614b18c9ec9c610d4e451534cb59f9
ericbdt <eric@LAPTOP-6DVD499O.localdomain>
2022-11-29 10:40:56 +0100

    réorganisation avec le makefile

---
+++ build/CMakeFiles/sfml-app.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/ericb/OneDrive/Bureau/ENSTA/2A/Projet IN204/Augment-par-le-numERIC/repository/src/main.cpp" "/mnt/c/Users/ericb/OneDrive/Bureau/ENSTA/2A/Projet IN204/Augment-par-le-numERIC/build/CMakeFiles/sfml-app.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/mnt/c/Users/ericb/OneDrive/Bureau/ENSTA/2A/Projet IN204/Augment-par-le-numERIC/repository/inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/sfml-app.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfml-app.dir/src/main.cpp.o"
+  "sfml-app"
+  "sfml-app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfml-app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
